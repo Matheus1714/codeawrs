119981860ab6ddb23cd6699e2f5c562e9bbd125c
matheus1714 <matheussilvamartins1714@gmail.com>
2022-03-03 23:27:14 -0300

    create a enum for browsers

---
+++ utils/enums/EnumBrowserCrawler.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class EnumBrowserCrawler(Enum):
+
+    chrome             = 'CHROME'
+    mozilla            = 'MOZILLA'
+    edge               = 'EDGE'
+    internet_explorer  = 'IE'
